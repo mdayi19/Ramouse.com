e7e2e08121642d9425d6b697a8d68a07cb9fcffd
majd dayi <majed.daya1994@gmail.com>
2026-01-14 08:40:07 +0300

    Fix Nginx CORS: handle OPTIONS preflight requests and forward API to Laravel

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-38bb0eb2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ardbi4alhr"
+    "revision": "0.v20b12beoog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
