85d8a48cacd0badd4963cb09da445e5838e45470
majd dayi <majed.daya1994@gmail.com>
2026-01-31 19:14:23 +0300

    🧹 CHORE: Remove temporary debug logging

--- Backend/app/Http/Controllers/ChatbotController.php
+++ Backend/app/Http/Controllers/ChatbotController.php
@@ -92,7 +92,7 @@ class ChatbotController extends Controller
                 ->toArray();
 
             // 4. Call Service with user context
-            \Illuminate\Support\Facades\Log::info('Sending history to Gemini: ' . json_encode($history));
+
 
             $responseContent = $aiService->sendMessage(
                 $history,
